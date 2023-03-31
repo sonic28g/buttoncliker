e8e2d72398944f578a2e2d9c9f6746d69e93beaf
sonic28g <gui28ss@gmail.com>
2023-03-31 14:50:41 +0100

    v1.2.1
    Fixed 2x button + block
    + Style changes

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let x = 0;
 let mul = 1; 
+let x2 = 0;
+
 function clicker(){
-    document.getElementById("double").innerHTML = "2x - 50 clk";
+
+    if(x2 == 0){
+        document.getElementById("double").innerHTML = "2x - 100 clk";
+    }
+    
     if(x >= 1000){
         window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); 
         document.getElementById("num").innerHTML = x.toString();
@@ -12,14 +18,18 @@ function clicker(){
                 
 }
 
-function changeMul(){
-    if(x >= 50){
-        mul = 2;
-        x -= 50;
-        document.getElementById("mult").innerHTML = "2";
-        document.getElementById("num").innerHTML = x.toString();
-    } else{
-        document.getElementById("double").innerHTML = "Invalid";
+function changeMul2(){
+    if(x2 == 0){
+        if(x >= 100){
+            mul = 2;
+            x -= 100;
+            document.getElementById("mult").innerHTML = "2";
+            document.getElementById("num").innerHTML = x.toString();
+            document.getElementById("double").innerHTML = "Purchased";
+            x2 = 1;
+        } else{
+            document.getElementById("double").innerHTML = "Invalid";
+        }
     }
     
 }
