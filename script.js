bd4f36b8065f0f82ced82f91e972601bcf7e7a03
sonic28g <gui28ss@gmail.com>
2023-03-31 14:01:23 +0100

    v1.2
    +Multiplier2

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 let x = 0;
-            function clicker(){
-                if(x >= 1000){
-                    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); 
-                    document.getElementById("num").innerHTML = x.toString();
-                }else{
-                    x += 1
-                    document.getElementById("num").innerHTML = x.toString();
-                }
+let mul = 1; 
+function clicker(){
+    document.getElementById("double").innerHTML = "2x - 50 clk";
+    if(x >= 1000){
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); 
+        document.getElementById("num").innerHTML = x.toString();
+    }else{
+        x += mul
+        document.getElementById("num").innerHTML = x.toString();
+    }
                 
-            }
+}
+
+function changeMul(){
+    if(x >= 50){
+        mul = 2;
+        x -= 50;
+        document.getElementById("mult").innerHTML = "2";
+        document.getElementById("num").innerHTML = x.toString();
+    } else{
+        document.getElementById("double").innerHTML = "Invalid";
+    }
+    
+}
+
