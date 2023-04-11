c50f0bf301e13a35864c2534adb3a3f477568e2c
sonic28g <gui28ss@gmail.com>
2023-04-11 10:21:27 +0100

    v1.3
    5x Button added

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let x = 0;
 let mul = 1; 
 let x2 = 0;
+let x5 = 0;
 
 function clicker(){
 
     if(x2 == 0){
         document.getElementById("double").innerHTML = "2x - 100 clk";
+        document.getElementById("quint").innerHTML = "Locked"
+    }
+
+    if(x2 == 1 && x5 == 0){
+        document.getElementById("quint").innerHTML = "5x - 500 clk";
     }
     
     if(x >= 1000){
@@ -26,6 +32,7 @@ function changeMul2(){
             document.getElementById("mult").innerHTML = "Multiplier: 2x";
             document.getElementById("num").innerHTML = x.toString();
             document.getElementById("double").innerHTML = "Purchased";
+            document.getElementById("quint").innerHTML = "5x - 500 clk";
             x2 = 1;
         } else{
             document.getElementById("double").innerHTML = "Invalid";
@@ -33,4 +40,22 @@ function changeMul2(){
     }
     
 }
+
+
+function changeMul5(){
+    if(x5 == 0 && x2 == 1){
+        if(x >= 500){
+            mul = 5;
+            x -= 500;
+            document.getElementById("mult").innerHTML = "Multiplier: 5x";
+            document.getElementById("num").innerHTML = x.toString();
+            document.getElementById("quint").innerHTML = "Purchased";
+            x5 = 1;
+        } else{
+            document.getElementById("quint").innerHTML = "Invalid";
+        }
+    }
+    
+}
+
 
